2674f26ff787b0150b5afc9a9959cd46c6999fe4
tuannnguyen <tuan.nguyen25@tiki.vn>
2023-03-05 20:12:15 +0700

    list request apis and handle copy curl

--- background.js
+++ background.js
@@ -2,12 +2,54 @@ importScripts('constants.js')
 
 chrome.webRequest.onBeforeRequest.addListener(
     async function (details) {
-        if (details.url.includes(PATTERN_DS_INSURANCE)) {
+        if (details.url.includes(PATTERN_DS_INSURANCE)
+            && !details.url.includes("/ds-insurance/v2/policies/transaction/list?code=")
+            && !details.url.includes("/ds-insurance/v2/policies/transaction/detail?transaction_id=")) {
             await chrome.storage.local.set({[generateUniqueKey()]: details.url})
         }
     },
     {urls: ["<all_urls>"]}
 );
+
+chrome.webRequest.onHeadersReceived.addListener(
+     async function (details) {
+         if (details.url.includes(PATTERN_DS_INSURANCE)
+             && !details.url.includes("/ds-insurance/v2/policies/transaction/list?code=")
+             && !details.url.includes("/ds-insurance/v2/policies/transaction/detail?transaction_id=")) {
+             let headers = details.responseHeaders
+             let infoRequest = details.statusCode + "|"
+             for (const header of headers) {
+                 if (header.name.toLowerCase() === X_REQUEST_ID) {
+                     infoRequest += header.value
+                     break
+                 }
+             }
+
+             await chrome.storage.local.set({[details.url]: infoRequest})
+         }
+     },
+    {urls: ["<all_urls>"]},
+    ['responseHeaders', 'extraHeaders']
+)
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    async function (details) {
+        if (details.url.includes(PATTERN_DS_INSURANCE)
+            && !details.url.includes("/ds-insurance/v2/policies/transaction/list?code=")
+            && !details.url.includes("/ds-insurance/v2/policies/transaction/detail?transaction_id=")) {
+            let curlCommand = "curl '" + details.url + "'";
+
+            details.requestHeaders.forEach(function (header) {
+                curlCommand += " -H '" + header.name + ": " + header.value + "'";
+            });
+
+            await chrome.storage.local.set({[details.url + "-curl"]: curlCommand})
+        }
+    },
+    {urls: ["<all_urls>"]},
+    ["requestHeaders"]
+);
+
 
 function generateUniqueKey() {
     const prefix = PATTERN_API_NAME
@@ -16,14 +58,8 @@ function generateUniqueKey() {
 }
 
 // load main website
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-//     if(changeInfo.status === COMPLETE) {
-//         chrome.storage.local.get(null, function(items) {
-//             for (let element of Object.keys(items)) {
-//                 if (element.includes(PATTERN_API_NAME)) {
-//                     chrome.storage.local.remove(element)
-//                 }
-//             }
-//         });
-//     }
-// })
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    if (changeInfo.status === LOADING) {
+        chrome.storage.local.clear()
+    }
+})
--- popup.js
+++ popup.js
@@ -33,17 +33,20 @@ window.addEventListener("load", async (event) => {
 
     // check URL
     const currentURL = await getCurrentTabUrl()
-    if (!currentURL.includes("https://beta.tala.xyz/")) {
+    if (!(currentURL.includes(PREFIX_URL_DEV) || currentURL.includes(PREFIX_URL_PROD))) {
         alert(INVALID_WEBSITE_MSG)
         return
     }
 
+    const prefixURL = currentURL.includes(PREFIX_URL_DEV) ? PREFIX_URL_DEV : PREFIX_URL_PROD
+    const prefixAPI = currentURL.includes(PREFIX_URL_DEV) ? PREFIX_API_DEV : PREFIX_API_PROD
+
     // handle
-    chrome.tabs.query({
+    await chrome.tabs.query({
         active: true,
         currentWindow: true
-    }, function (tabs) {
-        chrome.tabs.sendMessage(
+    }, async function (tabs) {
+        await chrome.tabs.sendMessage(
             tabs[0].id,
             {
                 from: POPUP_SCREEN,
@@ -58,27 +61,47 @@ window.addEventListener("load", async (event) => {
                     document.getElementById("input-token").setAttribute("value", response.access_token)
                     document.getElementById("input-customer-id").setAttribute("value", response.customer_id)
 
-                    if (currentURL.includes("https://beta.tala.xyz/bao-hiem-so/thong-tin-hop-dong")) {
+                    if (currentURL.includes(PREFIX_URL_DEV + "/bao-hiem-so/thong-tin-hop-dong") || currentURL.includes(PREFIX_URL_PROD + "/bao-hiem-so/thong-tin-hop-dong")) {
                         const bookingCode = currentURL.split("/")[5].split("?")[0]
-                        const orderCode = await getOrderCode(bookingCode, response)
+                        const orderCode = await getOrderCode(bookingCode, response, prefixURL, prefixAPI)
                         document.getElementById("input-order-code").setAttribute("value", orderCode)
                         document.getElementById("input-booking-code").setAttribute("value", bookingCode)
                         document.getElementById("input-policy-id").setAttribute("value", response.policy_id)
+                    } else {
+                        document.getElementById("input-order-code").setAttribute("value", NOT_FOUND_MSG)
+                        document.getElementById("input-booking-code").setAttribute("value", NOT_FOUND_MSG)
+                        document.getElementById("input-policy-id").setAttribute("value", NOT_FOUND_MSG)
                     }
                 }
             })
     })
 
     // set value list apis request
-    let listAPIs = ""
-    await chrome.storage.local.get(null, function(apis) {
-        let index = 1
-        for (let element of Object.keys(apis)) {
-            listAPIs += index.toString() + "." + apis[element] + "\n\n"
-            index++
+    await chrome.storage.local.get(null, async function (items) {
+        let trContent = ""
+        for (let element of Object.keys(items)) {
+            for (const element2 of Object.keys(items)) {
+                if (items[element] === element2) {
+                    let curlCommand = items[element] + "-curl"
+                    let statusAndRequestID = items[element2].split("|")
+                    trContent += `<tr><td><button type="button" class="btn btn-info" id=${curlCommand}>Copy</button></td><td>${items[element]}</td><td>${statusAndRequestID[0]}</td><td>${statusAndRequestID[1]}</td></tr>`
+
+                    break
+                }
+            }
         }
-        document.getElementById("list-apis-request").textContent = listAPIs
-    });
+        document.querySelector("#table-result>tbody").innerHTML = `<tbody>${trContent}</tbody>`
+
+        // handle list button
+        let listTR = document.querySelector("#table-result>tbody").getElementsByTagName("tr")
+        for (const trTag of listTR) {
+            let buttonID = trTag.getElementsByTagName("td")[0].getElementsByTagName("button")[0].id
+
+            document.getElementById(buttonID).addEventListener('click', function () {
+                copyCurl(buttonID, items)
+            })
+        }
+    })
 })
 
 async function copyText(id) {
@@ -92,19 +115,32 @@ async function copyText(id) {
     })
 }
 
+function copyCurl(curlCommand, items) {
+    for (let element of Object.keys(items)) {
+        for (const element2 of Object.keys(items)) {
+            if (items[element] === element2 && element2 + "-curl" === curlCommand) {
+                navigator.clipboard.writeText(items[curlCommand]).then(r => {
+                    console.log(r)
+                    alert("Copied !")
+                })
+            }
+        }
+    }
+}
+
 async function getCurrentTabUrl () {
     const tabs = await chrome.tabs.query({ active: true })
     return tabs[0].url
 }
 
-async function getOrderCode(bookingCode, accessTokenObject) {
+async function getOrderCode(bookingCode, accessTokenObject, prefixURL, prefixAPI) {
     // get transaction id
     const myHeaders = new Headers();
     myHeaders.append("authority", "api.tala.xyz");
     myHeaders.append("accept", "application/json, text/plain, */*");
     myHeaders.append("accept-language", "en-US,en;q=0.9,vi;q=0.8,es;q=0.7");
-    myHeaders.append("origin", "https://beta.tala.xyz");
-    myHeaders.append("referer", "https://beta.tala.xyz/bao-hiem-so/lich-su-giao-dich/" + bookingCode)
+    myHeaders.append("origin", prefixURL);
+    myHeaders.append("referer", prefixURL + "/bao-hiem-so/lich-su-giao-dich/" + bookingCode)
     myHeaders.append("sec-fetch-dest", "empty");
     myHeaders.append("sec-fetch-mode", "cors");
     myHeaders.append("sec-fetch-site", "same-site");
@@ -118,7 +154,7 @@ async function getOrderCode(bookingCode, accessTokenObject) {
     };
 
     let transactionID = ""
-    await fetch("https://api.tala.xyz/ds-insurance/v2/policies/transaction/list?code=" + bookingCode, requestOptions)
+    await fetch(prefixAPI + "/ds-insurance/v2/policies/transaction/list?code=" + bookingCode, requestOptions)
         .then(response => response.json())
         .then(result => {
             transactionID = result.data.list[0].history[0].transaction_id
@@ -132,8 +168,8 @@ async function getOrderCode(bookingCode, accessTokenObject) {
     myHeadersTransactionDetail.append("authority", "api.tala.xyz");
     myHeadersTransactionDetail.append("accept", "application/json, text/plain, */*");
     myHeadersTransactionDetail.append("accept-language", "en-US,en;q=0.9,vi;q=0.8,es;q=0.7");
-    myHeadersTransactionDetail.append("origin", "https://beta.tala.xyz");
-    myHeadersTransactionDetail.append("referer", "https://beta.tala.xyz/bao-hiem-so/thong-tin-giao-dich/" + transactionID + transactionID + "/transaction")
+    myHeadersTransactionDetail.append("origin", prefixURL);
+    myHeadersTransactionDetail.append("referer", prefixURL + "/bao-hiem-so/thong-tin-giao-dich/" + transactionID + transactionID + "/transaction")
     myHeadersTransactionDetail.append("sec-fetch-dest", "empty");
     myHeadersTransactionDetail.append("sec-fetch-mode", "cors");
     myHeadersTransactionDetail.append("sec-fetch-site", "same-site");
@@ -147,7 +183,7 @@ async function getOrderCode(bookingCode, accessTokenObject) {
     };
 
     let orderCode = ""
-    await fetch("https://api.tala.xyz/ds-insurance/v2/policies/transaction/detail?transaction_id=" + transactionID + "&type=transaction", requestOptionsTransactionDetail)
+    await fetch(prefixAPI + "/ds-insurance/v2/policies/transaction/detail?transaction_id=" + transactionID + "&type=transaction", requestOptionsTransactionDetail)
         .then(response => response.json())
         .then(result => {
             for (const element of result.data.details) {
